80c718dd082e87345b23da4a5b80a607be0412c3
denise <silentfront@gmail.com>
2019-05-30 21:44:22 -0700

    enable asserts if debug is active

--- Quartz/src/Quartz/Core.h
+++ Quartz/src/Quartz/Core.h
@@ -11,6 +11,9 @@
 	#error Quartz only supports Windows!
 #endif
 
+#ifdef QZ_DEBUG
+    #define QZ_ENABLE_ASSERTS
+#endif
 
 #ifdef QZ_ENABLE_ASSERTS
 	#define QZ_ASSERT(x, ...) { if(!(x)) { QZ_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
@@ -21,3 +24,5 @@
 #endif
 
 #define BIT(x) (1 << x)
+
+#define QZ_BIND_EVENT_FN(fn) std::bind(&fn, this, std::placeholders::_1)
